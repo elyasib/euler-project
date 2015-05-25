e92aad8d18c1925cce3c8f9b2929bcc7f6fd450e
elyasib <elyasib.isc@gmail.com>
2015-05-25 18:55:42 -0500

    Update problem3.scala

--- problem3.scala
+++ problem3.scala
@@ -7,16 +7,15 @@ object problem3 {
   def largestFactor(num: BigInt): BigInt = {
     var pfc: Int = 2 //Prime factor candidate
     val limit: Int = ceil(sqrt(num.longValue())).toInt
-    var largestPF: Long = 2
+	  val fltb: BigInt = 2 //Base for Fermats Little Theorem primality test
     var isPrime: Boolean = false
-    
     while (pfc <= limit && !isPrime) {
       if (num % pfc == 0) // is num divisible by pfc? 
-        if (BigInt(2).pow(pfc).-(2) % pfc == 0) //is pfc a composite number? 
+        if (((fltb pow pfc) - 2) % pfc == 0) //is pfc a composite number? 
           isPrime = (2 until ceil(sqrt(pfc)).toInt) forall (pfc % _ != 0) //is pfc a prime number 
       pfc += 1
     }
-    if (isPrime) largestFactor(num / (pfc - 1)) else num * 1
+    if (isPrime) largestFactor(num / (pfc - 1)) else num
   }
   def main(args: Array[String]) {
     println(largestFactor(BigInt(600851475143L)).toString)
